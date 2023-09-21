527c5bd60dc83a565129eaf883f520140b8e4c1a
d1000so <d1000so@unicamp.br>
2023-09-20 21:35:25 -0300

    create class contador

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,12 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroDois = terminal.nextInt();
+    }
+}
